9aff8edbb588b60f7f3d0da6924de2fbb7c91164
Thin tran <trvathin@gmail.com>
2021-05-12 13:01:49 +0700

    Update

--- ProjectWF/FormMain.Designer.cs
+++ ProjectWF/FormMain.Designer.cs
@@ -43,8 +43,6 @@ namespace ProjectWF
             this.giớiThiệuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.dgvOrderDetail = new System.Windows.Forms.DataGridView();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -59,11 +57,13 @@ namespace ProjectWF
             this.btnDelOrderItem = new System.Windows.Forms.Button();
             this.cbCustomers = new System.Windows.Forms.ComboBox();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.menuStrip1.SuspendLayout();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvOrderDetail)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQuantity)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -74,7 +74,7 @@ namespace ProjectWF
             this.trợGiúpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1145, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1170, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -165,7 +165,7 @@ namespace ProjectWF
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 18);
+            this.label1.Location = new System.Drawing.Point(28, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 13);
             this.label1.TabIndex = 1;
@@ -174,49 +174,18 @@ namespace ProjectWF
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 55);
+            this.label2.Location = new System.Drawing.Point(28, 62);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Ngày:";
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.btnAdd);
-            this.panel1.Controls.Add(this.cbCustomers);
-            this.panel1.Controls.Add(this.dateTimePickerOrder);
-            this.panel1.Controls.Add(this.txtTotalOrder);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(728, 33);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(405, 174);
-            this.panel1.TabIndex = 3;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.btnDelOrderItem);
-            this.panel2.Controls.Add(this.btnAddOrderItem);
-            this.panel2.Controls.Add(this.cbProduct);
-            this.panel2.Controls.Add(this.numericUpDownQuantity);
-            this.panel2.Controls.Add(this.txtNote);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Controls.Add(this.label5);
-            this.panel2.Controls.Add(this.label6);
-            this.panel2.Controls.Add(this.dgvOrderDetail);
-            this.panel2.Location = new System.Drawing.Point(12, 33);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(710, 266);
-            this.panel2.TabIndex = 0;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // dgvOrderDetail
             // 
             this.dgvOrderDetail.AllowUserToAddRows = false;
             this.dgvOrderDetail.AllowUserToDeleteRows = false;
             this.dgvOrderDetail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvOrderDetail.Location = new System.Drawing.Point(286, 15);
+            this.dgvOrderDetail.Location = new System.Drawing.Point(290, 19);
             this.dgvOrderDetail.MultiSelect = false;
             this.dgvOrderDetail.Name = "dgvOrderDetail";
             this.dgvOrderDetail.ReadOnly = true;
@@ -228,7 +197,7 @@ namespace ProjectWF
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 94);
+            this.label3.Location = new System.Drawing.Point(28, 101);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(52, 13);
             this.label3.TabIndex = 3;
@@ -237,7 +206,7 @@ namespace ProjectWF
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 94);
+            this.label4.Location = new System.Drawing.Point(20, 98);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(47, 13);
             this.label4.TabIndex = 8;
@@ -246,7 +215,7 @@ namespace ProjectWF
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 18);
+            this.label5.Location = new System.Drawing.Point(20, 22);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(76, 13);
             this.label5.TabIndex = 6;
@@ -255,7 +224,7 @@ namespace ProjectWF
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(16, 55);
+            this.label6.Location = new System.Drawing.Point(20, 59);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(52, 13);
             this.label6.TabIndex = 7;
@@ -263,7 +232,7 @@ namespace ProjectWF
             // 
             // txtNote
             // 
-            this.txtNote.Location = new System.Drawing.Point(100, 94);
+            this.txtNote.Location = new System.Drawing.Point(104, 98);
             this.txtNote.MaxLength = 150;
             this.txtNote.Multiline = true;
             this.txtNote.Name = "txtNote";
@@ -272,7 +241,7 @@ namespace ProjectWF
             // 
             // numericUpDownQuantity
             // 
-            this.numericUpDownQuantity.Location = new System.Drawing.Point(100, 55);
+            this.numericUpDownQuantity.Location = new System.Drawing.Point(104, 59);
             this.numericUpDownQuantity.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -295,7 +264,7 @@ namespace ProjectWF
             // txtTotalOrder
             // 
             this.txtTotalOrder.Enabled = false;
-            this.txtTotalOrder.Location = new System.Drawing.Point(125, 94);
+            this.txtTotalOrder.Location = new System.Drawing.Point(139, 101);
             this.txtTotalOrder.Name = "txtTotalOrder";
             this.txtTotalOrder.Size = new System.Drawing.Size(260, 20);
             this.txtTotalOrder.TabIndex = 12;
@@ -304,7 +273,7 @@ namespace ProjectWF
             // 
             this.dateTimePickerOrder.CustomFormat = "dd/MM/yyyy";
             this.dateTimePickerOrder.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePickerOrder.Location = new System.Drawing.Point(125, 55);
+            this.dateTimePickerOrder.Location = new System.Drawing.Point(139, 62);
             this.dateTimePickerOrder.MaxDate = new System.DateTime(2100, 1, 1, 0, 0, 0, 0);
             this.dateTimePickerOrder.MinDate = new System.DateTime(2021, 1, 1, 0, 0, 0, 0);
             this.dateTimePickerOrder.Name = "dateTimePickerOrder";
@@ -316,14 +285,14 @@ namespace ProjectWF
             // 
             this.cbProduct.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbProduct.FormattingEnabled = true;
-            this.cbProduct.Location = new System.Drawing.Point(100, 15);
+            this.cbProduct.Location = new System.Drawing.Point(104, 19);
             this.cbProduct.Name = "cbProduct";
             this.cbProduct.Size = new System.Drawing.Size(158, 21);
             this.cbProduct.TabIndex = 11;
             // 
             // btnAddOrderItem
             // 
-            this.btnAddOrderItem.Location = new System.Drawing.Point(19, 224);
+            this.btnAddOrderItem.Location = new System.Drawing.Point(23, 228);
             this.btnAddOrderItem.Name = "btnAddOrderItem";
             this.btnAddOrderItem.Size = new System.Drawing.Size(106, 23);
             this.btnAddOrderItem.TabIndex = 12;
@@ -333,7 +302,7 @@ namespace ProjectWF
             // 
             // btnDelOrderItem
             // 
-            this.btnDelOrderItem.Location = new System.Drawing.Point(152, 224);
+            this.btnDelOrderItem.Location = new System.Drawing.Point(156, 228);
             this.btnDelOrderItem.Name = "btnDelOrderItem";
             this.btnDelOrderItem.Size = new System.Drawing.Size(106, 23);
             this.btnDelOrderItem.TabIndex = 13;
@@ -345,14 +314,14 @@ namespace ProjectWF
             // 
             this.cbCustomers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbCustomers.FormattingEnabled = true;
-            this.cbCustomers.Location = new System.Drawing.Point(127, 15);
+            this.cbCustomers.Location = new System.Drawing.Point(141, 22);
             this.cbCustomers.Name = "cbCustomers";
             this.cbCustomers.Size = new System.Drawing.Size(258, 21);
             this.cbCustomers.TabIndex = 16;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(17, 135);
+            this.btnAdd.Location = new System.Drawing.Point(31, 228);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(368, 23);
             this.btnAdd.TabIndex = 14;
@@ -360,13 +329,47 @@ namespace ProjectWF
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDelOrderItem);
+            this.groupBox1.Controls.Add(this.dgvOrderDetail);
+            this.groupBox1.Controls.Add(this.btnAddOrderItem);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.cbProduct);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.numericUpDownQuantity);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtNote);
+            this.groupBox1.Location = new System.Drawing.Point(12, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(710, 273);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh sách mua";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnAdd);
+            this.groupBox2.Controls.Add(this.cbCustomers);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.dateTimePickerOrder);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtTotalOrder);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(728, 33);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(430, 273);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thanh toán";
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1145, 461);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(1170, 339);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.menuStrip1);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -374,12 +377,12 @@ namespace ProjectWF
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvOrderDetail)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQuantity)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -401,8 +404,6 @@ namespace ProjectWF
         private System.Windows.Forms.ToolStripMenuItem sảnPhẩmToolStripMenuItem;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.DataGridView dgvOrderDetail;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DateTimePicker dateTimePickerOrder;
@@ -417,5 +418,7 @@ namespace ProjectWF
         private System.Windows.Forms.Button btnDelOrderItem;
         private System.Windows.Forms.ComboBox cbCustomers;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
